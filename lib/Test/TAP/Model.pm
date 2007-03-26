b9aa913cda1481ffd08166ae382da80e2cbf34e3
Yuval Kogman <nothingmuch@woobling.org>
2007-03-26 21:18:08 +0000

    Fix Todd Hepler's SUPER::new bug

--- lib/Test/TAP/Model.pm
+++ lib/Test/TAP/Model.pm
@@ -86,7 +86,7 @@ sub new_with_tests {
 	my $pkg = shift;
 	my @tests = @_;
 
-	my $self = $pkg->SUPER::new;
+	my $self = $pkg->new;
 	$self->run_tests(@tests);
 
 	$self;
@@ -96,7 +96,7 @@ sub new_with_struct {
 	my $pkg = shift;
 	my $meat = shift;
 
-	my $self = $pkg->SUPER::new(@_);
+	my $self = $pkg->new(@_);
 	$self->{meat} = $meat; # FIXME - the whole Test::Harness::Straps model can be figured out from this
 
 	$self;
